3683b50b0c4ff05688200218c7f42dd11a646ef2
Harmain Rizwan <harmainrizwan98@outlook.com>
2025-02-12 15:44:26 +0500

    initiazlied and started working

---
+++ src/assets/index.js
@@ -0,0 +1,20 @@
+import kevinRushLogo from './kevinRushLogo.png';
+import about from './about.jpg';
+import kevinRushProfileJpg from './kevinRushProfile.jpg';
+import kevinRushProfilePng from './kevinRushProfile.png';
+
+import project1 from "./projects/project-1.jpg"
+import project2 from "./projects/project-2.jpg"
+import project3 from "./projects/project-3.jpg"
+import project4 from "./projects/project-4.jpg"
+
+export {
+    kevinRushLogo,
+    about,
+    kevinRushProfileJpg,
+    kevinRushProfilePng,
+    project1,
+    project2,
+    project3,
+    project4
+}
