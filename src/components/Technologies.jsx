9a322c782be036c09fc91d9ec8617e5eb0a89483
Harmain Rizwan <88616082+Harmain89@users.noreply.github.com>
2025-02-13 10:58:03 +0500

    animation skill icons.

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -1,32 +1,93 @@
 import { RiReactjsLine } from "react-icons/ri";
+import { motion } from "motion/react";
+
+const iconVariants = (duration) => ({
+  initial: { y: -10 },
+  animate: {
+    y: [10, -10],
+    transition:  {
+      duration: duration,
+      ease: "linear",
+      repeat: Infinity,
+      repeatType: "reverse"
+    }
+  }
+})
 
 function Technologies() {
   return (
     <div className="border-b border-neutral-800 pb-24">
-      <h2 className="my-20 text-center text-4xl">Technologies</h2>
-      <div className="flex flex-wrap items-center justify-center gap-4">
-        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+      <motion.h2 
+        whileInView={{ opacity: 1, x: 0 }}
+        initial={{ opacity: 0, x: -100 }}
+        transition={{ duration: 1.5 }}
+        className="my-20 text-center text-4xl"
+      >
+        Technologies
+      </motion.h2>
+      <motion.div 
+        whileInView={{ opacity: 1, x: 0 }}
+        initial={{ opacity: 0, x: -100 }}
+        transition={{ duration: 1.5 }}
+        className="flex flex-wrap items-center justify-center gap-4"
+      >
+        <motion.div 
+          variants={iconVariants(2.5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
           <img src="https://skillicons.dev/icons?i=laravel" alt="Laravel" className="text-7xl text-cyan-400" />
-        </div>
-        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+        </motion.div>
+        <motion.div 
+          variants={iconVariants(3)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
           <img src="https://skillicons.dev/icons?i=react" alt="React" className="text-7xl text-cyan-400" />
-        </div>
-        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+        </motion.div>
+        <motion.div 
+          variants={iconVariants(3.5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
           <img src="https://skillicons.dev/icons?i=vue" alt="Vue.js" className="text-7xl text-cyan-400" />
-        </div>
-        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+        </motion.div>
+        <motion.div 
+          variants={iconVariants(5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
           <img src="https://skillicons.dev/icons?i=mysql" alt="Mysql" className="text-7xl text-cyan-400" />
-        </div>
-        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+        </motion.div>
+        <motion.div 
+          variants={iconVariants(2)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
           <img src="https://skillicons.dev/icons?i=mongodb" alt="MongoDB" className="text-7xl text-cyan-400" />
-        </div>
-        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+        </motion.div>
+        <motion.div 
+          variants={iconVariants(3)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
           <img src="https://skillicons.dev/icons?i=nodejs" alt="NodeJs" className="text-7xl text-cyan-400" />
-        </div>
-        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+        </motion.div>
+        <motion.div 
+          variants={iconVariants(2.5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
           <img src="https://skillicons.dev/icons?i=redux" alt="Redux" className="text-7xl text-cyan-400" />
-        </div>
-      </div>
+        </motion.div>
+      </motion.div>
     </div>
   );
 }
